3cd5cca71a12e90c4f5c94c27c0ba634d50e4868
Akingbesote Babajide <akingbesotebabajide@gmail.com>
2026-06-10 22:23:45 +0100

    initial commit

--- config.py
+++ config.py
@@ -1,9 +1,12 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "YOUR_BOT_TOKEN_HERE")
 CHANNEL_USERNAME = os.environ.get("CHANNEL_USERNAME", "@your_channel")
 RSS_URL = os.environ.get("RSS_URL", "https://9janetmovies.com.ng/rss")
-POST_INTERVAL = int(os.environ.get("POST_INTERVAL") or 3600)
-POSTS_PER_CYCLE = int(os.environ.get("POSTS_PER_CYCLE") or 5)
-DATABASE_PATH = os.environ.get("DATABASE_PATH", "bot.db")
+POST_INTERVAL = 3600
+POSTS_PER_CYCLE = 5
+DATABASE_PATH = os.path.join(os.path.dirname(__file__), "bot.db")
 SECRET_KEY = os.environ.get("SECRET_KEY", "change-this-secret")
