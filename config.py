8b804bf5afcf3d67d9b061777165f5b5890421a9
Akingbesote Babajide <akingbesotebabajide@gmail.com>
2026-06-13 23:21:50 +0100

    Add multi-c

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ CHANNEL_USERNAMES = [
 
 RSS_URL = os.environ.get("RSS_URL", "https://9janetmovies.com.ng/rss")
 POST_INTERVAL = 3600
-POSTS_PER_CYCLE = 5
+POSTS_PER_CYCLE = 50
 DATABASE_PATH = os.path.join(os.path.dirname(__file__), "bot.db")
 SECRET_KEY = os.environ.get("SECRET_KEY", "change-this-secret")
 TMDB_API_KEY = os.environ.get("TMDB_API_KEY", "my-key")
